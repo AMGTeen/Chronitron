c7b62c7c01affd3c65668186faefe06f65a98b59
Chip Thrasher <thechipthrasher@gmail.com>
2016-12-05 20:02:40 -0500

    Update chronitron.js

--- chronitron.js
+++ chronitron.js
@@ -1,11 +1,20 @@
 //
 // Chronitron.js
+// Uses jQuery, but you can use vanilla JS if you like.
 //
 
-function parsewords(){
-  
-  
-  
-}  
+$(document).ready(function() {
+  $('input').focus(); // Focus the input field
+});
 
-// Do your code here
+// IGNORE ALL ABOVE THIS LINE
+
+function displayUserContent() {
+  // When user presses ENTER, add <div class="input"> with text.
+  $('.history').append('<div class="input">' + $('input').val() + '</div>'); // Add new <div class="input">
+  $('input').val(''); // Clear input field
+}
+
+function parse() {
+  // Parse user input
+}
