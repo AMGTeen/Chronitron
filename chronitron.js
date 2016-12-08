6dce202797b010aec5c92543acb5104a775976a4
kerbin111 <seq1235711@oru.edu>
2016-12-08 13:47:24 -0500

    Update chronitron.js

--- chronitron.js
+++ chronitron.js
@@ -2,20 +2,21 @@
 // Chronitron.js
 // Uses jQuery, but you can use vanilla JS if you like.
 //
-
-$(document).ready(function() {
-  $('input').focus(); // Focus the input field
-});
+//Sorry... just testing something.
+//$(document).ready(function() {
+//  $('input').focus(); // Focus the input field
+//});
 
 // IGNORE ALL ABOVE THIS LINE
 
 document.getElementById("Submit").onclick = parse();
 
-function displayUserContent() {
-  // When user presses ENTER, add <div class="input"> with text.
-  $('.history').append('<div class="input">' + $('input').val("hi") + '</div>'); // Add new <div class="input">
-  $('input').val(''); // Clear input field
-}
+//
+//function displayUserContent() {
+// When user presses ENTER, add <div class="input"> with text.
+//  $('.history').append('<div class="input">' + $('input').val("hi") + '</div>'); // Add new <div class="input">
+//  $('input').val(''); // Clear input field
+//}
 
 function parse() {
   alert("Button");
