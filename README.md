4e365d33488cbaea97e8f8ace05f7dbcb7cc7978
chipthrasher <chip@model100.in>
2017-05-19 09:14:42 -0400

    WOOHOO

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
 # Chronitron
 Chronitron is a web-based interactive chatterbot (chatbot) written in JavaScript.
 
-### Contributing Reference
-
-User input (typing) field: `<input>`  
-User input (display only) field: `<div class="input">`  
-Output field: `<div class="output">`
-Container for all texts/responses: `<div class="history">`
-
-**Important:** When replying with Chronitron's response, make sure to append a new `<div class="output">`. Also, create a new `<div class="input">` when the user presses Enter.
-
----
-
-View the Work in Progress at [amgteen.github.io/Chronitron](https://amgteen.github.io/Chronitron)  
-View the Wiki at [github.com/AMGTeen/Chronitron/wiki](https://github.com/AMGTeen/Chronitron/wiki)
+### Work In Progress! Check Back Soon :)
--- chronitron.js
+++ chronitron.js
@@ -1,27 +1,65 @@
-///
 //
-// Chronitron.js
-// Uses jQuery, but you can use vanilla JS if you like.
 //
-$(document).ready(function() {
-  $('input').focus(); // Focus the input field
-}
+//
+//  Chronitron.js
+//
+//
+//
 
-// IGNORE ALL ABOVE THIS LINE
-
-$("#submit").click(parse);
-
-function parse() {
-  $('.history').prepend('<div class="input">' + $('#input').val() + '</div>'); // When you say something, add it to the chat
-  $('#input').val();
-  
-  //$('.history').prepend('<div class="output">' + $('#output').val() + '</div>');
-  
-  // then... parse and reply
-}
-
-$(document).keypress(function(e) { // Press ENTER to submit
-    if(e.which == 13) {
-        parse();
-    } 
+$('.type').keypress(function() {
+  if (event.which == 13) {
+    output = '';
+    event.preventDefault();
+    var text = $('.type').val();
+    $('.history').prepend('<div class="you">' + text + '</div>');
+    $('.type').val('');
+    var output = parse(text);
+    $('.history').prepend('<div class="them">' + output + '</div>');
+  }
 });
+
+function parse(input) {
+  text = input.toLowerCase();
+  text = text.replace(/[.,\ \'\/#!$%\^&\*;:{}?=\-_`~()]/g,"");
+
+  var replacements = {
+    'hello':['Hey','Hello','Yo','Hey!','Hello!','Yo!','Heya','Hey There','Heyyy','What\'s up?','Eyy','Ayy','Hello there!'],
+    'hey':['Hey','Hello','Yo','Hey!','Hello!','Yo!','Heya','Hey There','Heyyy','What\'s up?','Eyy','Ayy','Hello there!'],
+    'yo':['Hey','Hello','Yo','Hey!','Hello!','Yo!','Heya','Hey There','Heyyy','What\'s up?','Eyy','Ayy','Hello there!'],
+    'heya':['Hey','Hello','Yo','Hey!','Hello!','Yo!','Heya','Hey There','Heyyy','What\'s up?','Eyy','Ayy','Hello there!'],
+    'hello there':['Hey','Hello','Yo','Hey!','Hello!','Yo!','Heya','Hey There','Heyyy','What\'s up?','Eyy','Ayy','Hello there!'],
+    'hi':['Hey','Hello','Yo','Hey!','Hello!','Yo!','Heya','Hi!'],
+    'whats up':['Not much!','Just sitting here waiting for you to type something ;)','Haha not much','Talking to you! :)','THE SKY','The Sky ;)','Haha hello!','Not much!','Not much :)','Not much, how about you?','Not much, how\'re you doing?'],
+    'wassup':['Not much!','Just sitting here waiting for you to type something ;)','Haha not much','Talking to you! :)','THE SKY','The Sky ;)','Haha hello!','Not much!','Not much :)','Not much, how about you?','Not much, how\'re you doing?'],
+    'suh':['Just sitting here waiting for you to type something ;)','Suh to you bro','Haha not much','Talking to you! :)','Haha hello!','Not much!','Not much :)','Not much, how about you?','Not much, how\'re you doing?'],
+  }
+  for(var i = 0;i < Object.keys(replacements).length; i++) {
+    var index = Object.keys(replacements)[i];
+    var value = replacements[index];
+    if(index == text) {
+      var items = value.length;
+      var number = Math.floor(Math.random() * items);
+      var output = value[number];
+      return output;
+      return;
+    }
+  }
+  // IDK what you said?!!
+  if(text !== '') {
+    var idk = [
+      'Sorry, I\'m confused :(',
+      'Siri quote: I\'m not sure I understand! :D',
+      'Say what now what now',
+      'Pardon?',
+      'Could you please say that again?',
+      'Sorry, what do you mean?',
+      'I can only understand a few things, you know... so don\'t judge. Plus, I\'ll have more features soon anyway',
+      'You\'ve probably noticed I\'m a lame chatbot so I probably don\'t know what you mean :)'
+    ];
+      var idknumber = Math.floor(Math.random() * idk.length);
+      var output = idk[idknumber];
+      return output;
+  } else {
+    return '';
+  }
+}
